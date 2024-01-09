46c7d49ae00cb63933dc27adcf07bb05c7e8c0ad
Chris-Effah <akboateng41@gmail.com>
2024-01-09 13:12:24 -0800

    trial

--- expen-track-app.js
+++ expen-track-app.js
@@ -2,8 +2,20 @@
 let expenses = [];
 
 #adding expenses functionality
-docu.getElementById("expensesForm").addEventListener("submit", function(event) {
+document.getElementById("expensesForm").addEventListener("submit", function(event) {
     event.preventDefault();
     
-    const amount = doc.getElementById("amount").value.trim();
-    const description =
+    const amount = document.getElementById("amount").value.trim();
+    const description = document.getElementById("description").value.trim();
+    const category = document.getElementById("category").value.trim();
+
+    if (amount && description && category) {
+        addExpense(amount, description, category);
+        document.getElementById("amount").value = "";
+        document.getElementById("description").value = "";
+        document.getElementById("category").value = "";
+    } else {
+      alert("Please fill in all fields!");
+    }
+});
+    
